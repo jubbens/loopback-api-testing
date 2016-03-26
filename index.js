afab7331539d395b0c236ec3e2e5e43fa6de91d3
Jordan Ubbens <jordan@shiverware.com>
2016-03-26 13:23:19 -0600

    added authenticated requests

--- index.js
+++ index.js
@@ -22,7 +22,6 @@ module.exports = {
 			server.close(done);
 		});
 
-
 		describe('Loopback API', function() {
 			for (var i = 0; i < conf.length; i++) {
 				var c = conf[i];
@@ -39,29 +38,69 @@ module.exports = {
 					return callback('Test has no expected response code specified');
 				}
 
-				var description = 'should respond '+c.expect+' on '+c.method+' requests to /'+c.model;
+				var isWithAuthentication = (c.hasOwnProperty('username') && c.hasOwnProperty('password'));
+				var authenticationDescription = (isWithAuthentication) ? 'authenticated' : 'unauthenticated';
 
+				var description = 'should respond '+c.expect+' on '+authenticationDescription+' '+c.method+' requests to /'+c.model;
 				var parsedMethod;
 
-				if (c.method === 'GET') {
+				var loginBlock = function(loginCallback) { 
+					return loginCallback(null, null); 
+				};
+
+				if (c.method.toUpperCase() === 'GET') {
 					parsedMethod = agent.get(baseURL+c.model);
-				} else if (c.method === 'POST') {
+				} else if (c.method.toUpperCase() === 'POST') {
 					parsedMethod = agent.post(baseURL+c.model);
+				} else if (c.method.toUpperCase() === 'PUT') {
+					parsedMethod = agent.put(baseURL+c.model);
+				} else if (c.method.toUpperCase() === 'DELETE') {
+					parsedMethod = agent.delete(baseURL+c.model);
 				}
 
 				if (typeof parsedMethod === 'undefined') {
 					return callback('Test has an unrecognized method type');
 				}
 
+				if (isWithAuthentication) {
+					loginBlock = function(loginCallback) {
+						agent
+						.post(baseURL+'users/login')
+				    .send({ email: c.username, password: c.password, ttl: '1209600000' })
+				    .set('Accept', 'application/json')
+				    .set('Content-Type', 'application/json')
+				    .expect(200)
+				    .end(function(err, authRes) {
+				      if (err) {
+				        return loginCallback('Could not log in with provided credentials', null);
+				      }
+
+				      var token = authRes.body.id;
+
+				      return loginCallback(null, token);
+				    });
+					}
+				}
+
 				it(description, function(done) {
-					parsedMethod
-					.expect(c.expect)
-					.end(function(err, res) {
-						if (err) {
-							return done(err);
-						} else {
-							return done();
+					loginBlock(function(loginError, loginToken) {
+						if (loginError) {
+							return done(loginError);
 						}
+
+						if (loginToken) {
+							parsedMethod = parsedMethod.set('Authorization', loginToken);
+						}
+
+						parsedMethod
+						.expect(c.expect)
+						.end(function(err, res) {
+							if (err) {
+								return done(err);
+							} else {
+								return done();
+							}
+						});
 					});
 				});
 			}
--- readme.md
+++ readme.md
@@ -1,6 +1,16 @@
 # Loopback API Testing #
 
-This package is a simplified replacement for [loopback-testing](https://github.com/strongloop/loopback-testing), which is now deprecated. It generates [Mocha](https://mochajs.org/) tests for API routes and examines their responses.
+This package is a simplified replacement for [loopback-testing](https://github.com/strongloop/loopback-testing), which is now considered deprecated. It generates [Mocha](https://mochajs.org/) tests for Loopback API routes and examines their responses.
+
+The main difference between this package and [loopback-testing](https://github.com/strongloop/loopback-testing) is that loopback-api-testing does not require you to write any code. Tests are specified in json format and the tests are generated automatically.
+
+This package is not supported by, endorsed by, or associated with Strongloop or the core Loopback team.
+
+# Installing #
+
+```
+npm install loopback-api-testing
+```
 
 # Example Usage #
 
@@ -33,4 +43,30 @@ Running the tests (for example):
 
 ```
 mocha --reporter spec test
+```
+
+Should get you:
+
+```
+  Loopback API
+    ✓ should respond 401 on unauthenticated GET requests to /Users (67ms)
+
+
+  1 passing (318ms)
+```
+
+# Making Authenticated Requests #
+
+You can specify a `username` and `password` in your tests to make the request as an authenticated user.
+
+```
+[
+	{
+		"method": "GET",
+		"model": "Cars",
+		"username": "my@user.com",
+		"password": "myPassword",
+		"expect": 200
+	}
+]
 ```

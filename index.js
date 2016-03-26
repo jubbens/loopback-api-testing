b0b96c849ab93b04720bdfbac1524e07e2d5a38a
Jordan Ubbens <jordan@shiverware.com>
2016-03-25 18:18:31 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,70 @@
+'use strict';
+
+var supertest = require('supertest');
+var agent = supertest.agent('http://localhost:3000');
+var app = require('../server/server');
+
+module.exports = {
+	run: function(confFile, callback) {
+		var server;
+		var conf = require(confFile);
+		var baseURL = '/api/';
+
+		if (typeof conf !== 'object') {
+			return callback('Failed to load test configuration from file');
+		}
+
+		before(function(done) {
+			server = app.listen(done);
+		});
+
+		after(function(done) {
+			server.close(done);
+		});
+
+
+		describe('Loopback API', function() {
+	  	for (var i = 0; i < conf.length; i++) {
+				var c = conf[i];
+
+				if (!c.hasOwnProperty('method')) {
+					return callback('Test has no method specified');
+				}
+
+				if (!c.hasOwnProperty('model')) {
+					return callback('Test has no route specified');
+				}
+
+				if (!c.hasOwnProperty('expect')) {
+					return callback('Test has no expected response code specified');
+				}
+
+				var description = 'should respond '+c.expect+' on '+c.method+' requests to /'+c.model;
+
+				var parsedMethod;
+
+				if (c.method === 'GET') {
+					parsedMethod = agent.get(baseURL+c.model);
+				} else if (c.method === 'POST') {
+					parsedMethod = agent.post(baseURL+c.model);
+				}
+
+				if (typeof parsedMethod === 'undefined') {
+					return callback('Test has an unrecognized method type');
+				}
+
+				it(description, function(done) {
+					parsedMethod
+			    .expect(c.expect)
+			    .end(function(err, res) {
+			      if (err) {
+			        return done(err);
+			      } else {
+			        return done();
+			      }
+			    });
+				});
+			}
+		});
+	}
+}
---
+++ readme.md
@@ -0,0 +1,36 @@
+# Loopback API Testing #
+
+This package is a simplififed replacement for [loopback-testing](https://github.com/strongloop/loopback-testing), which is now deprecated. It generates [Mocha](https://mochajs.org/) tests for API routes and examines their responses.
+
+# Example Usage #
+
+The test file (e.g. `test/loopbackAPI.test.js`)
+
+
+```
+var loopbackApiTesting = require('loopback-api-testing');
+
+loopbackApiTesting.run('./apiTestConfig.json', function(err) {
+	if (err) {
+  		console.log(err);
+  	}
+});
+```
+
+The test configuration JSON file (`apiTestConfig.json`):
+
+```
+[
+	{
+		"method": "GET",
+		"model": "Users",
+		"expect": 401
+	}
+]
+```
+
+Running the tests (for example):
+
+```
+mocha --reporter spec test
+```
